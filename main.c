420d5ab63ac8eceb53f39cfe1df55abb6a0a5b15
Baptiste Toussaint <bat.toussaint@gmail.com>
2022-05-05 16:04:10 +0200

    Premier commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/****************************************************************
+ * Ce projet a pour objectif de simuler le système industriel de Sy15
+ * Voir le README.md pour plus d'informations
+ * *************************************************************/
+
+
